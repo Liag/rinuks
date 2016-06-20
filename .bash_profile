1219166689a589b2b64486bd95dd0bdc1b871bf9
Liag <liag247@gmail.com>
2016-06-20 22:42:54 +0200

    Migration to fish, added arch logo ascii on login.

--- .bash_profile
+++ .bash_profile
@@ -4,4 +4,4 @@
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 #[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
-rmssay
+cat ~/prog/rinuks/arch_logo.txt
---
+++ config.fish
@@ -0,0 +1,23 @@
+env -i HOME=$HOME dash -l -c printenv | sed -e '/PWD/d; /PATH/s/:/ /g;s/=/ /;s/^/set -x /' | source
+
+set fish_greeting
+
+#touhourmssay
+if status --is-login
+	cat ~/prog/rinuks/arch_logo.txt
+end
+
+function ls --description 'List contents of directory'
+	set -l param --color=auto 
+		if isatty 1
+			set param $param --indicator-style=classify
+		end 
+		command ls $param $argv 
+end
+
+function ll --description 'List contents of directory using long format'
+        env COLLATE=C ls --group-directories-first --color=auto -alhF $argv
+end
+
+
+ 
