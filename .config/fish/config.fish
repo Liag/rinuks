aa777d1ae463ae3ad1a1ab73e90c7d2e21ce5297
Liag <liag247@gmail.com>
2017-09-05 00:30:40 +0200

    Added several functions to the fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,8 +21,34 @@ function ll --description 'List contents of directory using long format'
         env LC_COLLATE=C ls --group-directories-first --color=auto -alhF $argv
 end
 
+function irc --description "connect to server and attach to weechat tmux"
+	start_agent
+	ssh isa -t tmux attach -t irc
+end
+
+function hathlog
+	start_agent
+	ssh isa -t hathlog -f
+end
+
+function syns --description "Start synergy server"
+	synergys -f --enable-crypto
+end
+
+function synyr --description "Start synergy client and connect to yr"
+	synergyc -f --enable-crypto yr
+end
+
 function gitssh --description 'Add git ssh key'
 	start_agent_git
+end
+
+function pixivutil
+	python2 /home/v/prog/PixivUtil2/PixivUtil2.py
+end
+
+function sortpixiv
+	/home/v/bin/sortpixiv.sh
 end
 
 #start_agent
