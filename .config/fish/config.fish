9f946fa8ef00de50cd67894dcb31b9dd121eddca
Liag <liag247@gmail.com>
2017-10-15 03:58:02 +0200

    Disabled X start from terminal, added hg to gitignore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,8 +54,8 @@ end
 #start_agent
 
 # start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
-        exec startx -- -keeptty
-    end
-end
+#if status --is-login
+#    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+#        exec startx -- -keeptty
+#    end
+#end
